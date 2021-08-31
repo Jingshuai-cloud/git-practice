87160a9aecc4e0f92d839eabefd14666adafd254
Ada <ada20171218@gmail.com>
2021-08-31 20:03:12 +0930

    this is ada

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
         <p>This is commit by eva</p>
         <p>This is commit by shuaishuai again</p>
+=======
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> This is commit by Ada.</p>
+>>>>>>> this is ada
         <a
           className="App-link"
           href="https://reactjs.org"
